bf5de34c4d517e99faa56e16759c80e19233b1a6
home3751 <home3751@naver.com>
2026-05-11 17:57:24 +0900

    0511

---
+++ scrapper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+keyword = "파이썬"
+
+url = f"http://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+lis = soup.find_all("li", class_="c_col")
+
+# print(len(lis))
+# print(lis[0])
+
+for li in lis[0:1]:
+    # company = li.find("a", class_="cpname").text.strip()
+    # title = li.find("div",class_="cell_mid").find("div", class_="cl_top").find("a").text.strip()
+    # location = li.find("div",class_="cl_md").find_all("span")[0].text.strip()
+    link = li.find("div",class_="cell_mid").find("div","cl_top").find("a").get("href")
+    print(link)
+
